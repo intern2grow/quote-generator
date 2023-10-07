c0804ed572c532ae46ace433820f2cec89b119c7
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-10-07 16:12:21 +0100

    Code refactoring:Links

--- src/utils/social-links.js
+++ src/utils/social-links.js
@@ -1,18 +1,13 @@
 const socialLinks = (quote, platform) => {
   const sharedQuote = quote.author + " once said: " + quote.content;
 
-  const facebook = "https://www.facebook.com/sharer/sharer.php?u=&quote=";
-  const twitter = "https://twitter.com/intent/tweet?text=";
-  const whatsapp = "https://api.whatsapp.com/send?text=";
+  const map = {
+    facebook: "https://www.facebook.com/sharer/sharer.php?u=",
+    twitter: "https://twitter.com/intent/tweet?text=",
+    whatsapp: "https://wa.me/?text=",
+  };
 
-  const link =
-    platform === "facebook"
-      ? facebook
-      : platform === "twitter"
-      ? twitter
-      : whatsapp;
-
-  window.open(link + encodeURIComponent(sharedQuote), "_blank");
+  window.open(map[platform] + encodeURIComponent(sharedQuote), "_blank");
 };
 
 export default socialLinks;
